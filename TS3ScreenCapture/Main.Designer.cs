7627ff3d171acdf79a454c6f0e0f45b4961f2f3d
jonasp <jonasp@six.de>
2015-11-04 17:35:00 +0100

    first init

---
+++ TS3ScreenCapture/Main.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace TS3ScreenCapture
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.picCapture = new System.Windows.Forms.PictureBox();
+            this.lblFps = new System.Windows.Forms.Label();
+            this.timerFps = new System.Windows.Forms.Timer(this.components);
+            this.lblFP10S = new System.Windows.Forms.Label();
+            this.timerFP10S = new System.Windows.Forms.Timer(this.components);
+            this.pnlController = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numQuallity = new System.Windows.Forms.NumericUpDown();
+            this.numTab = new System.Windows.Forms.NumericUpDown();
+            this.numFactor = new System.Windows.Forms.NumericUpDown();
+            this.numSpeed = new System.Windows.Forms.NumericUpDown();
+            this.comboBoxClient = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picCapture)).BeginInit();
+            this.pnlController.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numQuallity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTab)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numFactor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSpeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 14);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 31);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnCapture.Enabled = false;
+            this.btnCapture.Location = new System.Drawing.Point(311, 12);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(75, 33);
+            this.btnCapture.TabIndex = 1;
+            this.btnCapture.Text = "Capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // picCapture
+            // 
+            this.picCapture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picCapture.Location = new System.Drawing.Point(0, 86);
+            this.picCapture.Name = "picCapture";
+            this.picCapture.Size = new System.Drawing.Size(886, 457);
+            this.picCapture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picCapture.TabIndex = 2;
+            this.picCapture.TabStop = false;
+            // 
+            // lblFps
+            // 
+            this.lblFps.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblFps.AutoSize = true;
+            this.lblFps.Location = new System.Drawing.Point(820, 9);
+            this.lblFps.Name = "lblFps";
+            this.lblFps.Size = new System.Drawing.Size(39, 13);
+            this.lblFps.TabIndex = 3;
+            this.lblFps.Text = "FPS: 0";
+            // 
+            // timerFps
+            // 
+            this.timerFps.Enabled = true;
+            this.timerFps.Interval = 1000;
+            this.timerFps.Tick += new System.EventHandler(this.timerFps_Tick);
+            // 
+            // lblFP10S
+            // 
+            this.lblFP10S.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblFP10S.AutoSize = true;
+            this.lblFP10S.Location = new System.Drawing.Point(820, 32);
+            this.lblFP10S.Name = "lblFP10S";
+            this.lblFP10S.Size = new System.Drawing.Size(51, 13);
+            this.lblFP10S.TabIndex = 4;
+            this.lblFP10S.Text = "FP10S: 0";
+            // 
+            // timerFP10S
+            // 
+            this.timerFP10S.Enabled = true;
+            this.timerFP10S.Interval = 10000;
+            this.timerFP10S.Tick += new System.EventHandler(this.timerFP10S_Tick);
+            // 
+            // pnlController
+            // 
+            this.pnlController.Controls.Add(this.label3);
+            this.pnlController.Controls.Add(this.label2);
+            this.pnlController.Controls.Add(this.label1);
+            this.pnlController.Controls.Add(this.numQuallity);
+            this.pnlController.Controls.Add(this.numTab);
+            this.pnlController.Controls.Add(this.numFactor);
+            this.pnlController.Controls.Add(this.numSpeed);
+            this.pnlController.Controls.Add(this.comboBoxClient);
+            this.pnlController.Controls.Add(this.btnConnect);
+            this.pnlController.Controls.Add(this.lblFP10S);
+            this.pnlController.Controls.Add(this.btnCapture);
+            this.pnlController.Controls.Add(this.lblFps);
+            this.pnlController.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlController.Location = new System.Drawing.Point(0, 0);
+            this.pnlController.Name = "pnlController";
+            this.pnlController.Size = new System.Drawing.Size(886, 86);
+            this.pnlController.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(706, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Speed:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(707, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Factor:";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(703, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Quallity:";
+            // 
+            // numQuallity
+            // 
+            this.numQuallity.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numQuallity.Location = new System.Drawing.Point(753, 59);
+            this.numQuallity.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            -2147483648});
+            this.numQuallity.Name = "numQuallity";
+            this.numQuallity.Size = new System.Drawing.Size(61, 20);
+            this.numQuallity.TabIndex = 9;
+            this.numQuallity.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            // 
+            // numTab
+            // 
+            this.numTab.Location = new System.Drawing.Point(93, 21);
+            this.numTab.Name = "numTab";
+            this.numTab.Size = new System.Drawing.Size(37, 20);
+            this.numTab.TabIndex = 8;
+            this.numTab.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numFactor
+            // 
+            this.numFactor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numFactor.Location = new System.Drawing.Point(753, 33);
+            this.numFactor.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numFactor.Name = "numFactor";
+            this.numFactor.Size = new System.Drawing.Size(61, 20);
+            this.numFactor.TabIndex = 7;
+            this.numFactor.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // numSpeed
+            // 
+            this.numSpeed.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numSpeed.Location = new System.Drawing.Point(753, 7);
+            this.numSpeed.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numSpeed.Name = "numSpeed";
+            this.numSpeed.Size = new System.Drawing.Size(61, 20);
+            this.numSpeed.TabIndex = 6;
+            this.numSpeed.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // comboBoxClient
+            // 
+            this.comboBoxClient.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.comboBoxClient.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxClient.Enabled = false;
+            this.comboBoxClient.FormattingEnabled = true;
+            this.comboBoxClient.Location = new System.Drawing.Point(392, 19);
+            this.comboBoxClient.Name = "comboBoxClient";
+            this.comboBoxClient.Size = new System.Drawing.Size(196, 21);
+            this.comboBoxClient.TabIndex = 5;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 543);
+            this.Controls.Add(this.picCapture);
+            this.Controls.Add(this.pnlController);
+            this.Name = "Main";
+            this.Text = "TS3ScreenCapture";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.picCapture)).EndInit();
+            this.pnlController.ResumeLayout(false);
+            this.pnlController.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numQuallity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTab)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numFactor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSpeed)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.PictureBox picCapture;
+        private System.Windows.Forms.Label lblFps;
+        private System.Windows.Forms.Timer timerFps;
+        private System.Windows.Forms.Label lblFP10S;
+        private System.Windows.Forms.Timer timerFP10S;
+        private System.Windows.Forms.Panel pnlController;
+        private System.Windows.Forms.ComboBox comboBoxClient;
+        private System.Windows.Forms.NumericUpDown numSpeed;
+        private System.Windows.Forms.NumericUpDown numFactor;
+        private System.Windows.Forms.NumericUpDown numTab;
+        private System.Windows.Forms.NumericUpDown numQuallity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TS3ScreenCapture/Main.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TS3ClientQueryFramework;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+using System.Drawing.Drawing2D;
+using System.Text.RegularExpressions;
+
+namespace TS3ScreenCapture
+{
+    public partial class Main : Form
+    {
+        TS3Client client = new TS3Client();
+        TS3Models.Client me = null;
+        int fpsCount = 0;
+        int fp10sCount = 0;
+        Thread captureThread = null;
+        string[] records = null;
+        Regex startRegex = new Regex(@"START_BASE64_(\d*)");
+        Regex contentRegex = new Regex(@"CONTENT_BASE64_(\d*)_");
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (client.Connect())
+            {
+                if (client.CurrentHandlerId != numTab.Value)
+                    client.Use((int)numTab.Value);
+                me = client.GetWhoami();
+                comboBoxClient.DataSource = client.GetClientList();
+                client.ClientNotifyRegister(client.CurrentHandlerId, TS3Models.Notifications.notifytextmessage);
+                client.Notifier.OnTextMessage += Notifier_OnTextMessage;
+
+                btnConnect.Enabled = false;
+                numTab.Enabled = false;
+                btnCapture.Enabled = true;
+                comboBoxClient.Enabled = true;
+            }
+            else
+                MessageBox.Show("Error");
+        }
+
+        private void Notifier_OnTextMessage(TS3Models.TextMessage textMessage)
+        {
+            if (textMessage.Msg == null || !client.IsConnected())
+                return;
+            bool isValid = false;
+            this.Invoke((MethodInvoker)delegate
+            {
+                if (comboBoxClient.SelectedItem != null && textMessage.InvokerId == ((TS3Models.Client)comboBoxClient.SelectedItem).ClId)
+                    isValid = true;
+            });
+            if (isValid)
+            {
+                if (startRegex.IsMatch(textMessage.Msg.Substring(0, textMessage.Msg.Length > 30 ? 30 : textMessage.Msg.Length)))
+                {
+                    Match match = startRegex.Match(textMessage.Msg);
+                    if (match.Success)
+                    {
+                        records = new string[Convert.ToInt32(match.Groups[1].Value)];
+                    }
+                }
+                else if (textMessage.Msg == "END_BASE64")
+                {
+                    try
+                    {
+                        string base64Image = string.Empty;
+                        foreach (string s in records)
+                        {
+                            base64Image += s;
+                        }
+
+                        byte[] imagebytes = Convert.FromBase64String(base64Image);
+
+                        MemoryStream ms = new MemoryStream(imagebytes);
+                        Image image = Image.FromStream(ms);
+
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            picCapture.Image = image;
+                        });
+
+                        fpsCount++;
+                        fp10sCount++;
+                    }
+                    catch { }
+                }
+                else if (contentRegex.IsMatch(textMessage.Msg.Substring(0, textMessage.Msg.Length > 30 ? 30 : textMessage.Msg.Length)))
+                {
+                    Match match = contentRegex.Match(textMessage.Msg.Substring(0, textMessage.Msg.Length > 30 ? 30 : textMessage.Msg.Length));
+                    if (match.Success)
+                    {
+                        try
+                        {
+                            records[Convert.ToInt32(match.Groups[1].Value)] = TS3Helper.UnescapeString(textMessage.Msg.Remove(0, match.Length));
+                        }
+                        catch { }
+                    }
+                }
+            }
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            if (captureThread == null)
+                CaptureLoop((int)numSpeed.Value, (int)numFactor.Value, (int)numQuallity.Value);
+            else
+            {
+                captureThread.Abort();
+                captureThread = null;
+            }
+        }
+
+        private void CaptureLoop(int speed = 1000, int scallFactor = 2, int quallity = 30)
+        {
+            captureThread = new Thread(delegate ()
+            {
+                int clid = me.ClId;
+                this.Invoke((MethodInvoker)delegate
+                {
+                    clid = ((TS3Models.Client)comboBoxClient.SelectedItem).ClId;
+                });
+                while (true)
+                {
+                    Bitmap bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                                   Screen.PrimaryScreen.Bounds.Height,
+                                   PixelFormat.Format32bppRgb); //Format32bppArgb
+
+                    // Create a graphics object from the bitmap.
+                    Graphics gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+
+                    // Take the screenshot from the upper left corner to the right bottom corner.
+                    gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                                                Screen.PrimaryScreen.Bounds.Y,
+                                                0,
+                                                0,
+                                                Screen.PrimaryScreen.Bounds.Size,
+                                                CopyPixelOperation.SourceCopy);
+
+                    Bitmap bImage = bmpScreenshot; //Image
+                    if (quallity >= 0)
+                        bImage = CompressImage(bImage, quallity);
+                    if (scallFactor > 1)
+                        bImage = ResizeImage(bmpScreenshot, Screen.PrimaryScreen.Bounds.Width / scallFactor, Screen.PrimaryScreen.Bounds.Height / scallFactor);
+
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        if(clid != me.ClId)
+                            picCapture.Image = (Image)bImage.Clone();
+                    });
+
+                    MemoryStream ms = new MemoryStream();
+                    bImage.Save(ms, ImageFormat.Jpeg);
+                    byte[] byteImage = ms.ToArray();
+                    string base64 = Convert.ToBase64String(byteImage); //Get Base64
+                    
+                    Queue<string> sends = new Queue<string>();
+                    int length = 1000;
+                    int splitsCount = 0;
+                    while (!string.IsNullOrEmpty(base64))
+                    {
+                        if (base64.Length < length)
+                            length = base64.Length;
+                        string output = base64.Substring(0, length);
+                        sends.Enqueue("CONTENT_BASE64_" + splitsCount + "_" + output);
+                        base64 = base64.Remove(0, length);
+                        splitsCount++;
+                    }
+
+                    client.SendTextMessage(TS3Models.TextMessageTargetMode.TextMessageTarget_CLIENT, clid, "START_BASE64_" + splitsCount, true);
+
+                    foreach (string s in sends)
+                    {
+                        client.SendTextMessage(TS3Models.TextMessageTargetMode.TextMessageTarget_CLIENT, clid, s, true);
+                    }
+                    
+                    client.SendTextMessage(TS3Models.TextMessageTargetMode.TextMessageTarget_CLIENT, clid, "END_BASE64", true);
+
+                    if (speed > 0)
+                        Thread.Sleep(speed);
+                    client.ClearReadBuffer();
+                }
+            });
+            captureThread.Start();
+        }
+
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            if (width == 0 || height == 0)
+                return null;
+
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+
+        private static Bitmap CompressImage(Image sourceImage, int imageQuality)
+        {
+            try
+            {
+                //Create an ImageCodecInfo-object for the codec information
+                ImageCodecInfo jpegCodec = null;
+
+                //Set quality factor for compression
+                EncoderParameter imageQualitysParameter = new EncoderParameter(
+                            System.Drawing.Imaging.Encoder.Quality, imageQuality);
+
+                //List all avaible codecs (system wide)
+                ImageCodecInfo[] alleCodecs = ImageCodecInfo.GetImageEncoders();
+
+                EncoderParameters codecParameter = new EncoderParameters(1);
+                codecParameter.Param[0] = imageQualitysParameter;
+
+                //Find and choose JPEG codec
+                for (int i = 0; i < alleCodecs.Length; i++)
+                {
+                    if (alleCodecs[i].MimeType == "image/jpeg")
+                    {
+                        jpegCodec = alleCodecs[i];
+                        break;
+                    }
+                }
+
+                //Save compressed image
+                MemoryStream ms = new MemoryStream();
+                sourceImage.Save(ms, jpegCodec, codecParameter);
+                return (Bitmap)Image.FromStream(ms);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(captureThread != null)
+                captureThread.Abort();
+            captureThread = null;
+            client.Disconnect();
+            Environment.Exit(0);
+        }
+
+        private void timerFps_Tick(object sender, EventArgs e)
+        {
+            if (fpsCount == 0 && fp10sCount < 10 && fp10sCount > 0)
+                lblFps.Text = "FPS: 0," + fp10sCount;
+            else
+                lblFps.Text = "FPS: " + fpsCount;
+            fpsCount = 0;
+        }
+
+        private void timerFP10S_Tick(object sender, EventArgs e)
+        {
+            lblFP10S.Text = "FP10S: " + fp10sCount;
+            fp10sCount = 0;
+        }
+    }
+}
